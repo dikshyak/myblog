bb01b83e9ef132f4b9554c11fae15999ba9ec4bd
Dikshya Khadka <your-github-email@gmail.com>
2026-05-24 12:51:17 +0545

    Add tests and CI workflow

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class PostTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='tester', password='pass1234'
+        )
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='Test content here',
+            author=self.user,
+            published=True
+        )
+
+    def test_post_list_loads(self):
+        r = self.client.get('/posts/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_post_appears_in_list(self):
+        r = self.client.get('/posts/')
+        self.assertContains(r, 'Test Post')
+
+    def test_post_detail_loads(self):
+        r = self.client.get(f'/posts/{self.post.pk}/')
+        self.assertEqual(r.status_code, 200)
