1f9b367d0bb77ca7345c5b1908cffc974cb2b5d5
Dikshya Khadka <your@email.com>
2026-05-24 12:14:29 +0545

    Initial Django blog project

---
+++ posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display  = ['title', 'author', 'published', 'created']
+    list_filter   = ['published']
+    search_fields = ['title', 'content']
